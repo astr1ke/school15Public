1c65387889b7cd6989910af9072967206a161c3d
astrike <astrike_pro@users.noreply.github.com>
2018-05-30 17:40:56 +0300

    e=v0.3.1

--- resources/views/documentsAll.blade.php
+++ resources/views/documentsAll.blade.php
@@ -14,9 +14,9 @@
             <h4 style="text-align: center; margin-bottom: 25px">Список документации с кратким просмотром</h4>
             <?php $documents = \App\document::all();?>
             @foreach($documents as $document)
-                    <div style="background: whitesmoke; border-radius: 8px" >
+                    <div style="background: whitesmoke; border-radius: 8px; height: max-content; padding: 20px; margin-top: 25px" >
                         <p><h4 style="text-align:center">{{$document->title}}</h4></p>
-                        <div style="max-height: 200px;  overflow: auto; padding: 20px">
+                        <div style="background: white; max-height: 200px;  overflow: auto;padding-left: 60px; margin: 20px">
                             <p>{!!$document->content!!}</p>
                         </div>
                     @if (Auth::check() and Auth::user()->IsAdmin == 1)
