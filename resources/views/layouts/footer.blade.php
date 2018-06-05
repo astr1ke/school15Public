070afb8e7ab452b3e9e64b358ac679da12fefcea
astrike <astrike_pro@users.noreply.github.com>
2018-06-05 23:44:10 +0300

    e=ChangeHeaderBackGroundAll2

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -17,12 +17,6 @@
                 <div class="copyright">
                     &copy; Школа №15. Все права защищены.
                     <div class="credits">
-                        <!--
-                          All the links in the footer should remain intact.
-                          You can delete the links only if you purchased the pro version.
-                          Licensing information: https://bootstrapmade.com/license/
-                          Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Company
-                        -->
                         <a href="/"></a> by <a href="/">Дмитрий Сафонов</a>
                     </div>
                 </div>
